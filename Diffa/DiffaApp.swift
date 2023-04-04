cbf2a37ba31b1c3c77537ba1beebc72628bc06af
ahmadamedfz <ahmadfariz11@gmail.com>
2023-04-04 15:36:37 +0700

    Initial Commit

---
+++ Diffa/DiffaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiffaApp.swift
+//  Diffa
+//
+//  Created by Ahmad Fariz on 04/04/23.
+//
+
+import SwiftUI
+
+@main
+struct DiffaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
