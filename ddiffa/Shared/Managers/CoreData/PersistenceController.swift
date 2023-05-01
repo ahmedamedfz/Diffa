0b3b0419d55dfc4e1083f44cc885e0e60272a146
ahmadamedfz <ahmadfariz11@gmail.com>
2023-05-01 20:15:40 +0700

    fixCoreData

---
+++ ddiffa/Shared/Managers/CoreData/PersistenceController.swift
@@ -0,0 +1,29 @@
+//
+//  PersistanceController.swift
+//  ddiffa
+//
+//  Created by Ahmad Fariz on 01/05/23.
+//
+
+import Foundation
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+    
+    let container: NSPersistentContainer
+    
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "TestCRUD")
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+}
